16d7a660c7035c1e3d3fe6eb8ea331c3f17b6a72
githua976sys <githua976@gmail.com>
2026-07-13 10:48:17 +0300

    update settings.py

--- Photoproject/Photoproject/settings.py
+++ Photoproject/Photoproject/settings.py
@@ -88,7 +88,12 @@ DATABASES = {
     }
 }
 
+cloudinary.config(
+    cloud_name = config("CLOUDINARY_CLOUD_NAME"),
+    api_key = config("CLOUDINARY_API_KEY"),
+    api_secret = config("CLOUDINARY_API_SECRET")
 
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
